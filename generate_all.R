cf41a9268924d5a397dcb30ffc14637c1c7d01c4
Emmanuel Chassot <EChassot@iotc.local>
2025-10-03 09:44:12 +0400

    Updating ES for the SC meeting

--- generate_all.R
+++ generate_all.R
@@ -3,20 +3,20 @@ options(scipen = 100)
 
 # Period to be included in ES
 FIRST_YEAR = 1950
-LAST_YEAR = 2023
+LAST_YEAR = 2024
 LAST_5_YEARS = (LAST_YEAR-4):LAST_YEAR
 
 # ES NUMBERING
 ES_NUMBERING = data.frame(SPECIES_CODE = c("ALB", "BET", "SKJ", "YFT", "SBF", "BLT", "FRI", "KAW", "LOT", "GUT", "COM", "BLM", "BUM", "MLS", "SFA", "SWO", "BSH", "OCS", "SPL", "SMA", "FAL", "BTH", "PTH"), NUMBER = 1:23)
 
 # General parameters
-ES_SPECIES_CODE  = "SKJ"
+ES_SPECIES_CODE  = "ALB"
 REPORT_YEAR      = 2024
-MEETING          = "SC26"
+MEETING          = "SC28"
 ES_NUMBER        = sprintf("%02d", ES_NUMBERING[ES_NUMBERING$SPECIES_CODE == ES_SPECIES_CODE,  "NUMBER"])
-LANGUAGE         = "E"
+LANGUAGE         = "[E]"
 
-TITLE = paste0("IOTC-", REPORT_YEAR, "-", MEETING, "-ES", ES_NUMBER, "_", ES_SPECIES_CODE, "_", LANGUAGE)
+TITLE = paste0("IOTC-", REPORT_YEAR, "-", MEETING, "-ES", ES_NUMBER, "_", ES_SPECIES_CODE, "", LANGUAGE)
 
 # Create output folder if not available
 if (!dir.exists(paste0("./outputs/", ES_SPECIES_CODE, "/charts/")))
@@ -26,9 +26,9 @@ dir.create(paste0("./outputs/", ES_SPECIES_CODE, "/charts/"), recursive = TRUE)
 setwd("initialization")
 source("00_CORE.R")
 setwd("..")
- 
+
 # OFFICEDOWN
-render("rmd/00_DOCX.Rmd", 
-       output_dir    = paste0("outputs/", ES_SPECIES_CODE, "/"), 
+render("rmd/00_DOCX.Rmd",
+       output_dir    = paste0("outputs/", ES_SPECIES_CODE, "/"),
        output_file   = paste0(TITLE, ".docx")
        )
